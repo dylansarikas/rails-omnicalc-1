9f112f4a1a140a2a123003f1a2947479fe075a69
dylansarikas <dylan.sarikas@gmail.com>
2024-06-04 15:21:51 -0500

    adds routing and html for square results

--- app/controllers/squares_controller.rb
+++ app/controllers/squares_controller.rb
@@ -3,5 +3,11 @@ class SquaresController < ApplicationController
     def show
         render({:template => "calc_templates/square_new"})
     end
+    
+    def results
+        @num = params["number"]
+        @square = params["number"].to_f * params["number"].to_f
+        render({:template => "calc_templates/square_results"})
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+    get "/" => "squares#show"
     get "/square/new" => "squares#show"
+    get "/square/results" => "squares#results"
   
 end
